83490f84d409b3652e3041c94b572def2a5def1b
atharva11m <mrutyunjay11m@gmail.com>
2024-02-05 22:10:43 +0530

    third comit

---
+++ DynamicCompo/src/App.jsx
@@ -0,0 +1,16 @@
+import Hello from "./Hello"
+import Random from "./Random"
+
+function App() {
+return <div>
+<Hello></Hello>
+<Random></Random>
+<Random></Random>
+<Random></Random>
+<Random></Random>
+<Random></Random>
+<Random></Random>
+
+</div>
+}
+export default App
---
+++ DynamicCompo/src/Hello.jsx
@@ -0,0 +1,13 @@
+function Hello () {
+  let name='Atharva Malode';
+  let address=()=>{
+    return 'Shankar Nagar Pusad'
+  }
+  return <h3>
+    THIS IS FUTURE.<br></br>
+    I am {name}<br>
+    </br>
+    My Address:{address()}
+  </h3>
+}
+export default Hello;
